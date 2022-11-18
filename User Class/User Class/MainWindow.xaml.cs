296a707cf653ab4606b151a269db1e820c3c5949
Alex22891 <103401307+Alex22891@users.noreply.github.com>
2022-11-18 15:04:04 +0900

    Add files via upload

---
+++ User Class/User Class/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace User_Class
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User()
+            {
+                FirstName = tbFirstName.Text,
+                Surname = tbSurName.Text,
+                Patronymic = tbPatronymic.Text,
+                Birthday = tbBirthday.Text
+            };
+
+            tbInfo.Text = $"Имя: {tbFirstName.Text} \n Фамилия: {tbSurName.Text} \n Отчество: {tbPatronymic.Text} \n Дата рождения: {tbBirthday.Text}";
+        }
+    }
+}
---
+++ User Class/User Class/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Class
+{
+    class User
+    {
+        public string FirstName;
+        public string Surname;
+        public string Patronymic;
+        public string Birthday;
+    }
+}
